32a520e27bce9a91f80c7b0cc2c87c0a229e98a9
Yorn <yornclaudel@gmail.com>
2022-04-22 23:19:34 +0200

    almost there ,, weird behaviour when trying to randomize food's position

--- snake.asm
+++ snake.asm
@@ -1,15 +1,8 @@
 org 0x07C00
 
 
-mov ah, 0x0 ;Set video mode, it should clean the screen too
-mov al, 0x03
-int 0x10
 
-mov ah, 0x1
-mov cx, 0x2607
-int 0x10 ; Removing cursor
-
-mov ax, snake_base ; Address of working-area memory for the snake
+mov ax, 0x07E1 ; Address of working-area memory for the snake
 mov es, ax ; setting the extra segment
 
 mov ax, 0xb800 ; Segment for the VGA text mode buffer
@@ -17,10 +10,20 @@ mov fs, ax
 
 
 start: ; Init the game
+mov ax, 0x03 ; go vga text mode
+int 0x10
+
+mov ah, 0x1
+mov cx, 0x2607
+int 0x10 ; Removing cursor
+
+; Setting the default values
+mov [delta], word 0x1
+mov [snake_len], word 0x4
+
 call spawn_food
 call spawn_snake
 
-loop_start:
 check_input:
 	mov ah, 0x1
 	int 0x16
@@ -30,27 +33,37 @@ check_input:
 	check_left:
 	cmp al, 'a'
 	jne check_up
-	mov [delta], word -0x1
-	jmp input_end
+	mov si, word -0x1
+	jmp check_backward
 	check_up:
 	cmp al, 'w'
 	jne check_right
-	mov [delta], word -0x100
-	jmp input_end
+	mov si, word -0x100
+	jmp check_backward
 	check_right:
 	cmp al, 'd'
 	jne check_down
-	mov [delta], word 0x1
-	jmp input_end
+	mov si, word 0x1
+	jmp check_backward
 	check_down:
 	cmp al, 's'
 	jne input_end
-	mov [delta], word 0x100
-
+	mov si, word 0x100
+check_backward:
+	mov di, si
+	imul di, -1
+	cmp di, [delta]
+	je input_end
+	mov [delta], si
 input_end:
 	;clearing the snake
 	mov si, ' '
 	call print_snake
+handle_growth:
+	cmp [growth], byte 0
+	je update_snake
+	inc word [snake_len]
+	dec byte [growth]
 update_snake:
 	mov ax, [snake_len]
 	update_snake_loop:
@@ -68,14 +81,42 @@ update_end:
 	mov dx, [es:bx]
 	add dx, [delta]
 	mov [es:bx], dx ;shifting the head
-
 	;printing the snake
 	mov si, 'o'
 	call print_snake
+check_out_of_bounds:
+	cmp [es:0], byte 0
+	jl start
+	cmp [es:0], byte 79
+	jg start
+	cmp [es:1], byte 0
+	jl start
+	cmp [es:1], byte 24
+	jg start
+check_collisions:
+	mov ax, 1
+	mov dx, word [es:0]
+	check_collisions_loop:
+	mov bx, ax
+	imul bx, 2
+	cmp dx, word [es:bx]
+	je start
+	inc ax
+	cmp ax, [snake_len]
+	jne check_collisions_loop
+
+check_collision_food:
+	mov si, word [food]
+	mov di, word [es:0]
+	cmp di , si
+	jne print_food
+	add [growth], byte 4
+	call spawn_food
 
 print_food:
-	movzx bx, [foodx]
-	movzx cx, [foody]
+	mov dx, [food]
+	movzx bx, dl
+	movzx cx, dh
 	mov dx, 'X'
 	call put_char
 
@@ -84,8 +125,9 @@ wait_a_bit:
 	mov dx, 0xFFFF
 	mov cx, 0x0
 	int 0x15
-loop:
-	jmp loop_start
+
+; Go back to start
+jmp check_input
 
 ;
 ; Callable procedures
@@ -112,30 +154,37 @@ put_char:
 	ret
 
 spawn_snake:
-	mov byte [es:0], 15
-	mov byte [es:1], 12
-	mov byte [es:2], 14
-	mov byte [es:3], 12
-	mov byte [es:4], 13
-	mov byte [es:5], 12
+	mov word [es:0], 0x0b0f
+	mov word [es:2], 0x0b0e
+	mov word [es:4], 0x0b0d
+	mov word [es:6], 0x0b0c
 	ret
 
 spawn_food:
-	mov byte [foodx], 50
-	mov byte [foody], 12
+	mov bx, word 80
+	call random
+	mov [food + 1 ], byte 12
+	mov bx, word 25
+	call random
+	mov [food], byte 25
+	ret
+random:
+	push ax
+
+	xor ax, ax
+	int 0x1a
+	mov ax, cx
+	xor dx, dx
+	div bx
+	mov si, dx
+
+	pop ax
 	ret
 
-;shift_loop:
-;	cmp ax, si
-;	add ax, 2
-;	jne shift_loop
-;	ret
-
-foodx db 1
-foody db 2
+food dw 0
 delta dw 0x1
-snake_base dw 0x7E1
-snake_len dw 0x3
+snake_len dw 0x4
+growth db 0
 
 times 510-($-$$) db 0
 	db 0x55
